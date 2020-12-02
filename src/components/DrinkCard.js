8d8819205d2dc9a0dd683ee4770bc2e0e02f00a4
Finnschy <skylerfearless@yahoo.de>
2020-12-02 09:57:04 +0100

    pull von mirnel

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -3,7 +3,7 @@ const DrinkCard = (props) => {
     return (
         <section className="DrinkCard">
             <div className="DrinkCardDiv">
-                <img src={props.drinksData.strDrinkThumb} />
+                <img src={props.drinksData.strDrinkThumb} alt="alcoholic drink"/>
                 <p>{props.drinksData.strDrink}</p>
                 </div>
         </section>
